8a5b9a175b5f91c50348a475cd40e3139f8a76b5
Samatsss <144174632+Samatsss@users.noreply.github.com>
2024-02-06 18:42:17 +0300

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,260 @@
+//1.1
+
+// let a = -3
+// if ( a < 0 ) {
+//     console.log('короче число отрицательное')
+// } else {
+//     console.log('короче число положительное')
+// }
+
+
+//1.2
+
+// let str = 'hegngbjbj'
+// console.log(str.length)
+
+
+//1.3
+
+// let str = 'hegngbjbj'
+// console.log(str[8])
+
+
+//1.4
+
+// let a = 5
+// if (number % 2 == 0) {
+//     console.log('число четное')
+// } else {
+//     console.log('число нечетное')
+// }
+
+
+//1.5
+
+//let a = 'sygesr'
+//let b = 'sgaet'
+//if (a[0] === b[0]) {
+//    console.log('совпали')
+//} else {
+//    console.log('видима не совпали')
+//}
+
+//1.6
+
+//let a = 'sfhbsь'
+//l = a[a.length - 1]
+//if (l == 'ь') {
+//    console.log(a[a.length - 2])
+//} else {
+//    console.log(l)
+//}
+
+
+
+//2.1
+
+//let a = '8945893'
+//console.log(a[0])
+
+
+
+//2.2
+
+//let a = '364564'
+//l = a[a.length - 1]
+//console.log(l)
+
+
+//2.3
+
+//let a = '364564'
+//let l = parseInt(a[a.length - 1])
+//let g = parseInt(a[0])
+//    console.log( l + g )
+
+
+//2.4
+
+//let number = `98347`
+//console.log(number.length)
+
+
+//2.5
+
+//let a = `4834`
+//let b = `3843`
+//if ( a[0] == b[0] ) {
+//    console.log('намана')
+//} else {
+//    console.log('не намана')
+//}
+
+
+
+//3.1
+
+// let a = 'uewh'
+// if ( a.length > 1) {
+//     console.log(a[a.length - 2])
+// } else {
+//     console.log(a)
+// }
+
+
+//3.2
+
+// let a = `4834`
+// let b = `3`
+// if ( a % b == 0 ) {
+//     console.log('делится')
+// } else {
+//     console.log('не делится')
+// }
+
+
+//4.1
+
+// let num = 101
+// for (var i = 1; i < num; i++) {
+//     console.log(i)
+// }
+
+
+//4.2
+
+// let num = 1
+// for (var i = -100; i < num; i++) {
+//     console.log(i)
+// }
+
+
+//4.3
+
+// let num = 0
+// for (var i = 100; i > num; i--) {
+//     console.log(i)
+// }
+
+
+//4.4
+
+// let num = 101
+// for (var i = 1; i < num; i++) {
+//     if ( i % 2 == 0 ) {
+//         console.log(i)
+//     }
+// }
+
+
+//4.5
+
+// let num = 101
+// for (var i = 1; i < num; i++) {
+//     if ( i % 3 == 0 ) {
+//         console.log(i)
+//     }
+// }
+
+
+//5.1
+
+// let num = 0;
+// for (let i = 1; i <= 100; i++) {
+// 	num = num + i;
+// }
+// console.log(num)
+
+
+//5.2
+
+// let num = 0;
+// for (let i = 2; i <= 100; i += 2) {
+//   num += i;
+// }
+// console.log(num);
+
+
+//5.3
+
+// let num = 0;
+// for (let i = 1; i <= 100; i += 2) {
+//   num += i;
+// }
+// console.log(num)
+
+
+//5.4
+
+// let a = 30
+// let b = 5
+// if ( a % b == 0) {
+//     console.log('остатка от деления нет')
+// } else {
+//     console.log( a % b)
+// }
+
+
+//5.5
+
+// let str = 'jdcb'
+// for (let i = str.length - 1; i > -1; i--) {
+//     console.log(str[i])
+// }
+
+
+//6.1
+
+// let arr = [2, 3, 4, 5, 6];
+// let num = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   num += arr[i] ** 2;
+// }
+// console.log(num)
+
+
+//6.2
+
+// let arr = [2, 3, 4, 5, 6];
+// let num = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   num += Math.sqrt(arr[i])
+// }
+// console.log(num)
+
+
+//6.3
+
+// let array = [1, -2, 3, -4, 5]
+// let num = 0;
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i] > 0) {
+//     num += array[i];
+//   }
+// }
+// console.log(num)
+
+
+//6.4
+
+// var numbers = [1, 5, 10, 15, 9, 3]
+// var sum = 0;
+// for (var i = 0; i < numbers.length; i++) {
+//   if (numbers[i] > 0 && numbers[i] < 10) {
+//     sum += numbers[i];
+//   }
+// }
+// console.log(sum)
+
+
+//7.1
+
+// const str = 'abcde';
+// const arr = [];
+// for(let i = 0; i < str.length; i++) {
+//   arr[i] = str[i];
+// }
+// console.log(arr);
+
+
+
